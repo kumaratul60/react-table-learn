509e00623afdebc348cf056ad1859bc37d3d4aad
Atul <atulreso1@gmail.com>
2022-11-13 11:44:04 +0530

    learning react-table

---
+++ src/components/Column.js
@@ -0,0 +1,30 @@
+import { format } from "date-fns";
+export const COLUMNS = [
+    {
+        Header: "id",
+        accessor: "id",
+    },
+    {
+        Header: "First Name",
+        accessor: "first_name",
+    },
+    {
+        Header: "Last Name",
+        accessor: "last_name",
+    },
+    {
+        Header: "Email",
+        accessor: "email",
+    },
+    {
+        Header: "DOB",
+        accessor: "date_of_birth",
+        Cell: ({ value }) => {
+            return format(new Date(value), "dd/MM/yyyy");
+        },
+    },
+    {
+        Header: "Age",
+        accessor: "age",
+    },
+];
